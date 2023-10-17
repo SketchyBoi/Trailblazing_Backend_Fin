d5d90c77be1ea8f2dff95dbdca69ab8e12cfffa5
Drew Reed <drewdafox@gmail.com>
2023-10-16 21:01:16 -0700

    user info works

--- src/main/java/com/nighthawk/spring_portfolio/mvc/usr/Usr.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/usr/Usr.java
@@ -1,9 +1,11 @@
 package com.nighthawk.spring_portfolio.mvc.usr;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collection;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -60,11 +62,13 @@ public class Usr {
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
 
-    private double highScore;
+    private ArrayList<HashMap<String, HashMap<String, Integer[][]>>> canvasHistory;
 
-    private double totalOfAllScores;
+    // private double highScore;
 
-    private int numberOfScores;
+    // private double totalOfAllScores;
+
+    // private int numberOfScores;
 
     // To be implemented
     @ManyToMany(fetch = EAGER)
@@ -81,16 +85,24 @@ public class Usr {
     @Type(type="json")
     @Column(columnDefinition = "jsonb")
     private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
 
     // Constructor used when building object from an API
     public Usr(String email, String password, String name) { //double highScore, double totalOfAllScores, int numberOfScores) {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.canvasHistory = new ArrayList<HashMap<String, HashMap<String, Integer[][]>>>();
         // this.highScore = highScore;
         // this.totalOfAllScores = totalOfAllScores;
         // this.numberOfScores = numberOfScores;
+    }
+
+    public void addCanvasHistory(HashMap<String, HashMap<String, Integer[][]>> newCanvasHistory) {
+        this.canvasHistory.add(newCanvasHistory);
+    }
+
+    public void addCanvasHistoryThruJSON(String newCanvasHistoryJSON) {
+        this.addCanvasHistory(convertJsonToData(newCanvasHistoryJSON));
     }
 
     // a custom getter to return the average score of a usr
@@ -102,35 +114,51 @@ public class Usr {
     //     return 0.0;
     // }
 
+    public static HashMap<String, HashMap<String, Integer[][]>> convertJsonToData(String jsonData) {
+        Gson gson = new Gson();
+        // type definition for parsing
+        TypeToken<HashMap<String, HashMap<String, Integer[][]>>> typeToken =
+                new TypeToken<HashMap<String, HashMap<String, Integer[][]>>>() {
+                };
+        // parsing JSON into correct data structure
+        HashMap<String, HashMap<String, Integer[][]>>
+                data = gson.fromJson(jsonData, typeToken.getType());
+
+        return data;
+    }
+
     // Initialize static test data 
     public static Usr[] init() {
 
         // basics of class construction
-        Usr p1 = new Usr();
-        p1.setName("Thomas Edison");
-        p1.setEmail("toby@gmail.com");
-        p1.setPassword("123Toby!");
+        Usr p1 = new Usr("toby@gmail.com", "123Toby!", "Thomas Edison");
         // p1.setHighScore(88.3);
         // p1.setTotalOfAllScores(238.2);
         // p1.setNumberOfScores(3);
 
         // basics of class construction
-        Usr p2 = new Usr();
-        p2.setName("The Master");
-        p2.setEmail("unbeat_table@gmail.com");
-        p2.setPassword("guessTHIS");
+        Usr p2 = new Usr("kendallreistemail@icloud.com", "kendallsImpossibleToGuessPassw0rd", "Kendall Reist");
         // p2.setHighScore(94.1);
         // p2.setTotalOfAllScores(1220.9);
         // p2.setNumberOfScores(14);
 
-        Usr p3 = new Usr();
-        p3.setName("Drew Reed");
-        p3.setEmail("drewreedyo@gmail.com");
-        p3.setPassword("notMyActualPassw0rd");
+        Usr p3 = new Usr("drewreedyo@gmail.com", "insecure", "Drew Reed");
+        // p3.setName("Drew Reed");
+        // p3.setEmail("drewreedyo@gmail.com");
+        // p3.setPassword("notMyActualPassw0rd");
+        HashMap<String, HashMap<String, Integer[][]>> data = new HashMap<>();
+        HashMap<String, Integer[][]> nodeIdData = new HashMap<>();
+        Integer[][] nodeData = {{10, 20}};
+        nodeIdData.put("1", nodeData);
+        HashMap<String, Integer[][]> adjacentData = new HashMap<>();
+        Integer[][] adjacentData1 = {{2, 30}, {3, 40}};
+        adjacentData.put("1", adjacentData1);
+        data.put("node_id", nodeIdData);
+        data.put("adjacent", adjacentData);
+        p3.addCanvasHistory(data);
         // p3.setHighScore(84.9);
         // p3.setTotalOfAllScores(500.0);
         // p3.setNumberOfScores(6);
-
         // Array definition and data initialization
         Usr usrs[] = {p1, p2, p3};
         return(usrs);
