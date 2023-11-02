527ecbb5e1d9c9850828ee6c40e115c58af88ecf
YLu-1258 <maodou1258@gmail.com>
2023-11-02 09:07:38 -0700

    add origin

--- src/main/java/com/nighthawk/spring_portfolio/mvc/usr/UsrApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/usr/UsrApiController.java
@@ -14,6 +14,7 @@ import com.nighthawk.spring_portfolio.mvc.usr.CanvasUpdate;
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "https://csa-tri-1.github.io")
 @RequestMapping("/api/usr")
 public class UsrApiController {
     @Autowired UsrDetailsService usrService;
